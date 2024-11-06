48e48daa079be1c20cf9e230fc00f16a93e7c4e5
geekyfaahad <77067677+geekyfaahad@users.noreply.github.com>
2024-11-06 14:29:37 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -100,135 +100,7 @@ function request(local) {
     });
 }
 
-// $(document).ready(function () {
-//   $("form").on("submit", function (event) {
-//     event.preventDefault();
 
-//     // Get the folder path from the form's action attribute
-//     var folderPath = $(this).attr("action");
-
-//     // Check if files are selected
-//     var fileInput = $(this).find('input[type="file"]');
-//     if (fileInput.get(0).files.length === 0) {
-//       Swal.fire({
-//         position: "top-end",
-//         icon: "warning",
-//         title: "No files selected",
-//         showConfirmButton: false,
-//         timer: 3000,
-//         width: 400,
-//       });
-//       return;
-//     }
-
-//     // Validate file sizes and types
-//     var validTypes = [
-//       "text/plain", // .txt
-//       "application/pdf", // .pdf
-//       "image/png", // .png
-//       "image/jpeg", // .jpg
-//       "image/jpeg", // .jpeg
-//       "image/gif", // .gif
-//       "video/mp4", // .mp4
-//       "video/x-matroska", // .mkv
-//       "application/zip", // .zip
-//     ];
-
-//     var maxFileSize = 500 * 1024 * 1024; // 500 MB limit
-
-//     var formData = new FormData($(this)[0]);
-//     var files = fileInput.get(0).files;
-
-//     for (var i = 0; i < files.length; i++) {
-//       if (files[i].size > maxFileSize) {
-//         Swal.fire({
-//           position: "top-end",
-//           icon: "warning",
-//           title: "File size exceeds 500 MB: " + files[i].name,
-//           showConfirmButton: false,
-//           timer: 3000,
-//           width: 400,
-//         });
-//         return;
-//       }
-//       if (!validTypes.includes(files[i].type)) {
-//         Swal.fire({
-//           position: "top-center",
-//           icon: "warning",
-//           title: "Invalid file type: " + files[i].name,
-//           showConfirmButton: false,
-//           timer: 2000,
-//           width: 400,
-//         });
-//         return;
-//       }
-//     }
-
-//     // Show a Swal modal with a progress bar
-//     Swal.fire({
-//       title: "Uploading...",
-//       html: '<div class="progress" style="width: 100%;"><div id="progressBar" class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;">0%</div></div>',
-//       allowOutsideClick: false,
-//       showConfirmButton: false,
-//       width: 400,
-//     });
-
-//     $.ajax({
-//       xhr: function () {
-//         var xhr = new window.XMLHttpRequest();
-//         xhr.upload.addEventListener("progress", function (e) {
-//           if (e.lengthComputable) {
-//             var percent = Math.round((e.loaded / e.total) * 100);
-//             $("#progressBar")
-//               .attr("aria-valuenow", percent)
-//               .css("width", percent + "%")
-//               .text(percent + "%");
-//           }
-//         });
-//         return xhr;
-//       },
-//       type: "POST",
-//       url: folderPath,
-//       data: formData,
-//       processData: false,
-//       contentType: false,
-//       beforeSend: function (xhr) {
-//         xhr.setRequestHeader(
-//           "X-CSRF-Token",
-//           $('meta[name="csrf-token"]').attr("content")
-//         );
-//       },
-//       success: function (response) {
-//         Swal.fire({
-//           position: "top-end",
-//           icon: "success",
-//           title: "Your data has been successfully uploaded",
-//           showConfirmButton: false,
-//           timer: 2000,
-//           width: 400,
-//         });
-
-//         // Wait for 3.5 seconds, then proceed
-//         setTimeout(function () {
-//           window.location.reload();
-//           $("form")[0].reset(); // Reset the form
-//           $("#progressBar").css("width", "0%").text(""); // Reset progress bar
-//         }, 3500);
-//       },
-//       error: function (xhr, status, error) {
-//         Swal.fire({
-//           position: "top-end",
-//           icon: "error",
-//           title: "An error occurred during the upload",
-//           text: error,
-//           showConfirmButton: false,
-//           timer: 3000,
-//           width: 400,
-//         });
-//       },
-//     });
-//   });
-// });
 
 function options(folder_path, filename, file_size) {
   Swal.fire({
@@ -341,8 +213,7 @@ $(document).ready(function () {
       }
     });
   });
-});
-
+});
 
 function delete_folder() {
   Swal.fire({
